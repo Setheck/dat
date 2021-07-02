e6e493dc0ec7ca9a14fb8a75ca4086f31ffaab31
seth <402885+Setheck@users.noreply.github.com>
2021-07-02 14:02:45 -0700

    skip clipboard test if unsupported

--- clipper_test.go
+++ clipper_test.go
@@ -3,10 +3,15 @@ package main
 import (
 	"testing"
 
+	"github.com/atotto/clipboard"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDefaultClipper(t *testing.T) {
+	if clipboard.Unsupported {
+		t.SkipNow()
+	}
 	testClipData := "some clipboard data"
 	if err := ClipboardHelper.WriteAll(testClipData); err != nil {
 		t.Fatal(err)
